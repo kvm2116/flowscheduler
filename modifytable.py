88836ece28051aaf613efde9935e1ecf3a82b829
Kunal Mahajan <kunalmahajan@mantessakm.cs.columbia.edu>
2016-12-13 17:11:48 -0500

    Removed comments

--- modifytable.py
+++ modifytable.py
@@ -48,21 +48,6 @@ def addflow(dpid, ipv4_src, ipv4_dst, tcp_src, tcp_dst, outport):
 	print result
 
 
-# command = "curl -X POST -d '{\"switch\": \"04:4d:2c:23:3a:3f:2a:93\", \"name\":\"flow-mod-9\", \"table\":\"200\"," + \
-#           "\"cookie\":\"0\", \"priority\":\"3\", \"ipv4_src\":\"10.10.1.1\", \"ipv4_dst\":\"10.10.1.2\", \"eth_type\":\"0x0800\"," + \
-#           "\"tcp_src\":\"5001\", \"tcp_dst\":\"5002\", \"ip_proto\":\"0x06\", \"active\":\"true\", \"actions\":\"output=47\"}'" + \
-#           " http://128.104.222.94:8080/wm/staticflowpusher/json"
-# result = os.popen(command).read()
-# print result
-# addflow("04:4d:2c:23:3a:3f:2a:93", "10.10.1.1", "10.10.1.2", "5001", "5002", "47")
-
-
-# Get all the static flows for the switch:
-# for i in range(len(switches)):
-#   command = "curl -s http://%s/wm/staticflowpusher/list/%s/json" % (controllerRestIP, switches[i]['switchDPID']) 
-#   flows = os.popen(command).read()
-#   print flows
-
 switch_dpid = "04:55:2c:23:3a:3f:0e:c4"
 def getflows():
 	allflows_command = "curl -s http://%s/wm/core/switch/all/flow/json" % (controllerRestIP)
@@ -85,9 +70,6 @@ def printflows(result,isAll):
 				print item
 	print("--- --- --- --- --- --- --- --- ---")
 
-# def deleteFlow():
-# 	delete_command = "curl -X DELETE -d '{"name":"flow-mod-1"}' http://<controller_ip>:8080/wm/core/switch/json"
-
 # Add output to controller flow in table 200
 command = "curl -X POST -d '{\"switch\": \"04:55:2c:23:3a:3f:0e:c4\", \"name\":\"flow-mod-8\", \"table\":\"200\"," + \
           "\"cookie\":\"0\", \"priority\":\"0\", \"active\":\"true\", \"actions\":\"output=controller\"}'" + \
@@ -98,17 +80,3 @@ while(True):
 	getflows()
 
 
-# eth_dst = "90:e2:ba:b3:ba:44"
-# command = "curl -X POST -d '{\"switch\":\"04:55:2c:23:3a:3f:37:60\", \"name\":\"flow-mod-1\", \"table\":\"100\"," + \
-#           "\"cookie\":\"0\", \"priority\":\"32768\", \"eth_dst\":\"90:e2:ba:b3:ba:44\", \"eth_type\":\"0x0800\","+ \
-#           "\"ipv4_dst\":\"10.10.1.2\", \"active\":\"true\", \"actions\":\"output=13\"}'" + \
-#           " http://128.104.223.10:8080/wm/staticflowpusher/json"
-# result = os.popen(command).read()
-# print result
-
-# command = "curl -X POST -d '{\"switch\": \"04:69:2c:23:3a:3f:42:69\", \"name\":\"flow-mod-9\", \"table\":\"200\"," + \
-#           "\"cookie\":\"0\", \"priority\":\"3\", \"ipv4_dst\":\"10.10.1.1\", \"ipv4_src\":\"10.10.1.2\", \"eth_type\":\"0x0800\"," + \
-#           "\"tcp_src\":\"5010\", \"tcp_dst\":\"5009\", \"active\":\"true\", \"actions\":\"output=56\"}'" + \
-#           " http://128.104.222.34:8080/wm/staticflowpusher/json"
-# result = os.popen(command).read()
-# print result
